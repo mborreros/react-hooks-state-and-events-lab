774eacc7ea9ff7d2f19a54d83c986e220629e510
Maya Borrero <mlbsindia@gmail.com>
2021-11-27 12:46:18 -0500

    commit tree a6768a32325ef6ebec774c85142e85f725cd9c75

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+// when the user clicks the <button> element, the item should be added to their virtual cart. The way we'll show the user that the item is in the cart is by changing the className on the <li> element
 
 function Item({ name, category }) {
+
+  const [ inCart, setState ] = useState(false)
+
+  let cartStatus = inCart ? "in-cart" : ""
+
+  let buttonText = inCart ? "Remove from " : "Add to "
+
+  let buttonClass = inCart ? "remove" : "add"
+
+  function addToCart() {       
+    setState(!cartStatus)
+  };
+
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={() => addToCart()}>{buttonText}Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [ selectedCategory, setState ] = useState(items)
+
+  function handleChange(event) {
+    let filteredList = items.filter((item) => (item.category === event.target.value || event.target.value === "All"))
+    setState(filteredList)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(event) => handleChange(event)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +21,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {selectedCategory.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
